16cf0f2bc09924a3565fbb7cd8e9a262810f6061
Ahlem <58731392+kawaremu@users.noreply.github.com>
2021-01-12 00:38:12 +0100

    Create RandomNumberGenerator.py
    Just a mixture of inspiration and maths. :)

---
+++ RandomNumberGenerator.py
@@ -0,0 +1,12 @@
+import os,time  #Let's import the OS and time library
+
+#We trying to make a random number generation out of scratch using real numbers from the computer of the hardware
+
+def randomNumber(suprema):
+  #The number we gonna return is gonna divide the processus ID by the time of the processor clock of the computer, modulo the number
+  #given by the user.
+  number = os.getpid()*time.time_ns()%suprema 
+  return int(number)
+
+
+print(randomNumber(456)) 

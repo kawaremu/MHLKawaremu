0a846a1ed3daba017d04f7a3ae2c4294e711dc7b
Ahlem <58731392+kawaremu@users.noreply.github.com>
2021-01-13 15:59:20 +0100

    Update DataVisualisation.py

--- DataVisualisation.py
+++ DataVisualisation.py
@@ -1,3 +1,15 @@
+import numpy as np
 import pandas as pd
-import matplotlib.plt as plt 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+data = pd.read_csv('vgsales.csv')
+data.head()
+
+drop_row_index = data[data['Year'] > 2015].index
+data = data.drop(drop_row_index)
+data['Genre'].value_counts()
+sns.countplot(x="Genre", data=data, order = data['Genre'].value_counts().index)
+plt.xlabel('Genre of the game')
+plt.ylabel('Sales by genre')
+plt.xticks(rotation=90)
